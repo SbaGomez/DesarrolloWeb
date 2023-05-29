8b08733f22147e34403d11801cc6b0c946c0540a
Sebastian Gomez <ssgomez@trabajo.gob.ar>
2023-05-29 13:54:42 -0300

    fixed height dinamico segun contenido

--- js/script.js
+++ js/script.js
@@ -21,58 +21,47 @@ document.addEventListener("DOMContentLoaded", function () {
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
                 document.getElementById("contenido").innerHTML = this.responseText;
-                ajustarAlturaContenido(); // Ajusta la altura del contenido después de cargar el nuevo contenido
+                ajustarAlturaContenido();
 
                 // Variable para titulo de la pagina
                 var tituloPagina = "";
 
-                // Verificar la URL actual
-                var esInicio = url.includes("inicio.html");
-                var esSomos = url.includes("somos.html");
-                var esIdeas = url.includes("ideas.html");
-                var esContacto = url.includes("contacto.html");
-
                 // Cambiar la clase y estilos del banner segun la URL
                 var banner = document.getElementById("banner");
                 var texto = document.getElementById("banner").querySelector("p");
-                var newsletter = document.getElementById("newsletter");
                 var titulo = document.getElementById("banner").querySelector("h1");
 
-                if (esInicio) {
+                if (url.includes("inicio.html")) {
                     banner.style.height = "400px";
                     banner.style.padding = "150px 40px";
                     texto.style.display = "block";
-                    //newsletter.style.display = "block";
                     titulo.textContent = "Proporción áurea y fractales";
                     titulo.classList.remove("tituloBanner");
                     banner.style.backgroundImage = "url('../img/seccion.jpg')";
                     tituloPagina = "Diseño y Desarrollo Web - Home";
                 }
-                else if (esSomos) {
+                else if (url.includes("somos.html")) {
                     banner.style.height = "200px";
                     banner.style.padding = "70px 20px";
                     texto.style.display = "none"
-                    //newsletter.style.display = "none";
                     titulo.textContent = "Diseño Web";
                     titulo.classList.add("tituloBanner");
                     banner.style.backgroundImage = "url('../img/header1.jpg')";
                     tituloPagina = "Diseño y Desarrollo Web - Somos";
                 }
-                else if (esIdeas) {
+                else if (url.includes("ideas.html")) {
                     banner.style.height = "200px";
                     banner.style.padding = "70px 20px";
                     texto.style.display = "none"
-                    //newsletter.style.display = "none";
                     titulo.textContent = "Ideas";
                     titulo.classList.add("tituloBanner");
                     banner.style.backgroundImage = "url('../img/ideas3.jpg')";
                     tituloPagina = "Diseño y Desarrollo Web - Ideas";
                 }
-                else if (esContacto) {
+                else if (url.includes("contacto.html")) {
                     banner.style.height = "200px";
                     banner.style.padding = "70px 20px";
                     texto.style.display = "none"
-                    //newsletter.style.display = "none";
                     titulo.textContent = "Contacto";
                     titulo.classList.add("tituloBanner");
                     banner.style.backgroundImage = "url('../img/header1.jpg')";
@@ -83,10 +72,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 document.title = tituloPagina;
 
                 // Obtener los elementos de navegación
-                var inicio = document.getElementById("inicio");
-                var somos = document.getElementById("somos");
-                var ideas = document.getElementById("ideas");
-                var contacto = document.getElementById("contacto");
+                var elementosNavegacion = document.querySelectorAll("#inicio, #somos, #ideas, #contacto");
 
                 // Función para activar el elemento de navegación correspondiente
                 function activarElemento(elemento) {
@@ -95,46 +81,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 // Función para desactivar todos los elementos de navegación
                 function desactivarElementos() {
-                    inicio.classList.remove("active");
-                    somos.classList.remove("active");
-                    ideas.classList.remove("active");
-                    contacto.classList.remove("active");
+                    elementosNavegacion.forEach(function (elemento) {
+                        elemento.classList.remove("active");
+                    });
                 }
 
                 // Asignar eventos de clic a los elementos de navegación
-                inicio.addEventListener("click", function () {
-                    desactivarElementos();
-                    activarElemento(inicio);
+                elementosNavegacion.forEach(function (elemento) {
+                    elemento.addEventListener("click", function () {
+                        desactivarElementos();
+                        activarElemento(elemento);
+                    });
                 });
-
-                somos.addEventListener("click", function () {
-                    desactivarElementos();
-                    activarElemento(somos);
-                });
-
-                ideas.addEventListener("click", function () {
-                    desactivarElementos();
-                    activarElemento(ideas);
-                });
-
-                contacto.addEventListener("click", function () {
-                    desactivarElementos();
-                    activarElemento(contacto);
-                });
-
             }
         };
         xhttp.open("GET", url, true);
         xhttp.send();
     }
 
-
     // Ajusta la altura del contenido según su contenido
     function ajustarAlturaContenido() {
         var contenido = document.getElementById("contenido");
-        contenido.style.height = "auto"; // Restablece la altura a "auto" para recalcularla correctamente
+        contenido.style.height = "350px"; // Restablece la altura a "auto" para recalcularla correctamente
         contenido.style.height = (contenido.scrollHeight + 120) + "px"; // Establece la altura según el contenido
-
     }
 
     cargarContenido("inicio.html");
